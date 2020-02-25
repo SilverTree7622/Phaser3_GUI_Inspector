22856a940fa514d7389a22ec2d1b848cb8d109a5
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-02-26 00:18:14 +0900

    add some another method

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 
 require('./gui/lib/DatGUILib.js');
 require('./gui/GUIClass.js');
+// require('./gui/GUIClass.js');
 // import * as dat from require('./gui/lib/DatGUILib.js');
 // import * as GUIClass from require('./gui/GUIClass.js');
+
+// Publish method
+window.PHASER3_DATGUI_INSPECTOR = window.PHASER3_DATGUI_INSPECTOR || {};
+window.PHASER3_DATGUI_INSPECTOR = GUIClass;
+console.log(window.PHASER3_DATGUI_INSPECTOR);
