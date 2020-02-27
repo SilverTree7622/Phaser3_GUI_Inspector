6c6513de1fc221afaf3afbfd1ba99971398beb25
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-02-27 15:02:10 +0900

    update check

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
-// Publish method
+// Publish method 1
 window.GUIClass = require('./gui/GUIClass.js').GUIClass;
+
+// Publish method 2
+// import * as GUIClass from require('./gui/GUIClass.js');
+
+// Publish method 3
+// require('./gui/GUIClass.js');
+
+// window.GUIClass = GUIClass;
 
 // check is really exist
 console.log('GUIClass in window:', window.GUIClass);
