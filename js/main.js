f54a5fed2f65342bbef66c82f02b6885f2524790
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2020-02-28 11:57:40 +0900

    rearrange to function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 
-// Publish method 1
-window.GUIClass = require('./gui/GUIClass.js').GUIClass;
+// function
+function PhaserDatActionWithScene (_scene, _statusManager) {
+    // declare
+    let tmpClass;
+    let tmpStatus;
+    let tmpReturn;
+    // value
+    tmpClass = InitSet();
+    tmpStatus = CheckSM(_statusManager);
+    tmpReturn = CommonAction(tmpClass, _scene);
+    // return
+    return tmpReturn;
+}
 
+// detail function
+function InitSet() {
+    let tmpClass = require('./gui/GUIClass.js').GUIClass;
+    return tmpClass;
+}
+function CheckSM(_statusManager) {
+    let tmpSM;
+    if (_statusManager) {} // status manager exist
+    else {} // status manager not exist
+    return tmpSM;
+}
+function CommonAction(_tmpClass, _scene) {
+    let GUIClass = new _tmpClass();
+    GUIClass.create(_scene);
+    // manage update(FPS)
+    return GUIClass;
+}
 
-console.log('require:', require('./gui/GUIClass.js'));
-console.log('window in lib:', window);
-console.log('GUIClass in window:', window.GUIClass);
-// Publish method 2
-// import * as GUIClass from require('./gui/GUIClass.js');
-
-// Publish method 3
-// require('./gui/GUIClass.js');
-
-// window.GUIClass = GUIClass;
-
-// check is really exist
+// ADD TO WINDOW
+window.PhaserDatActionWithScene = PhaserDatActionWithScene;
